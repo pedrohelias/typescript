5e61d1ea4de81365ae45d09a83f0f7ea34a5a34f
Pedro Helias <pedrohelias95@hotmail.com>
2021-09-05 15:18:30 -0300

    modificações

--- todo_type/src/App.tsx
+++ todo_type/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC, ChangeEvent, useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import {Itask} from "./interfaces"
+const App: FC = () => {
 
-function App() {
+  const [task, SetTask] = useState<string>("")
+  const [number1, SetNumber] = useState<number>(0)
+  const [task2, SetTask2] = useState<string>("")
+  const [task3, SetTask3] = useState<string>("")
+  const [task4, SetTask4] = useState<Itask[]>([]) //vai ser uma lista
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement> ):void => {
+    if(event.target.name === "task"){
+      SetTask(event.target.value)
+    }else {
+      SetNumber(Number(event.target.value))
+    }
+  }
+  
+  const AddTask = ():void => {
+    const newTask = {taskName: task, numero: number1, taskName2: task2, taskName3: task3}
+    SetTask4([...task4,newTask])
+    console.log(task4)
+    SetTask("")
+    SetNumber(0)
+    SetTask2("")
+    SetTask3("")
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <div className="inputContainer">
+          <input type="text" placeholder="texto aqui" name="task" onChange={handleChange} value={task}></input>
+          <input type="number" placeholder="numero aqui" name="numero" value = {number1}></input>
+          <input type="text" placeholder="texto aqui" name = "task2" value = {task2}></input>
+          <input type="text" placeholder="texto aqui" name = "task3" value = {task3}></input>
+          
+        </div>
+        <button onClick={AddTask} className="botao">Add Task</button>
+      </div>
+      <div className="todoList"></div>
+
+
     </div>
   );
 }
---
+++ todo_type/src/interfaces.tsx
@@ -0,0 +1,10 @@
+export interface Itask{
+
+    taskName: string;
+    numero: number;
+    taskName2: string;
+    taskName3: string;
+
+
+
+}
